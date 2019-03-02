15dfa9f0c94313afbac4f51746dd07dc426d11f9
danielpiesik <daniel.m.piesik@gmail.com>
2019-03-02 23:27:08 +0100

    added API documentation

--- bookstore/config/urls.py
+++ bookstore/config/urls.py
@@ -2,9 +2,13 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
+from rest_framework_swagger.views import get_swagger_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('documentation/', get_swagger_view(title='Bookstore API')),
+
     path('', include('authors.urls')),
     path('', include('books.urls')),
     path('', include('orders.urls')),
