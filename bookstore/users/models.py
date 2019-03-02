e33adebc9f4645276bfcb0e6fe46ed151322b081
danielpiesik <daniel.m.piesik@gmail.com>
2019-03-02 19:57:29 +0100

    added `Order` and `User` models

--- bookstore/users/models.py
+++ bookstore/users/models.py
@@ -3,3 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.set_password(self.password)
+        super().save(*args, **kwargs)
