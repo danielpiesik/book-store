7217a2a74edef4e4987a5f412c31119d9bbfec56
danielpiesik <daniel.m.piesik@gmail.com>
2019-03-03 22:18:31 +0100

    filter books by author, title, genre

--- bookstore/books/views.py
+++ bookstore/books/views.py
@@ -7,3 +7,4 @@ from .serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    filter_fields = ('authors', 'title', 'genre')
