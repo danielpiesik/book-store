ee2263b6e7a422cbaf5b8a12b6c27dfaa92d1924
danielpiesik <daniel.m.piesik@gmail.com>
2019-03-03 22:06:44 +0100

    set logged user as byuer

--- bookstore/orders/serializers.py
+++ bookstore/orders/serializers.py
@@ -19,6 +19,7 @@ class OrderSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         books = validated_data.pop('books', [])
         validated_data['price'] = sum(book.price for book in books)
+        validated_data['buyer'] = self.context['request'].user
         instance = Order.objects.create(**validated_data)
         instance.books.add(*books)
         return instance
